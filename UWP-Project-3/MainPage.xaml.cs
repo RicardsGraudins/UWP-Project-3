4a4d692d34e83b27ee969ad823c60591ccf2f0d2
Ricards <G00314715@gmit.ie>
2017-11-17 23:33:36 +0000

    GPS Exception

--- UWP-Project-3/MainPage.xaml.cs
+++ UWP-Project-3/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using Windows.Devices.Geolocation;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using System.Diagnostics;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -21,25 +22,51 @@ namespace UWP_Project_3
             this.InitializeComponent();
         }//MainPage
 
+        //Adapted from https://docs.microsoft.com/en-us/uwp/api/windows.devices.geolocation.geolocator
+        //             https://docs.microsoft.com/en-us/windows/uwp/maps-and-location/get-location
         public class locationManager
         {
             public async static Task<Geoposition> GetPosition()
             {
+                try
+                {
                     var accessStatus = await Geolocator.RequestAccessAsync();
-                    if (accessStatus != GeolocationAccessStatus.Allowed) throw new Exception();
+                    if (accessStatus != GeolocationAccessStatus.Allowed)
+                    {
+                        throw new GPSException("GPS not enabled.");
+                    }
                     var geolocator = new Geolocator { DesiredAccuracyInMeters = 0 };
                     var position = await geolocator.GetGeopositionAsync();
                     return position;
+                }//try
+                catch(GPSException)
+                {
+                    Debug.WriteLine("GPS not enabled.");
+                    return null;
+                }//catch
             }//GetPosition
         }//locationManager
 
         private async void button_Click(object sender, RoutedEventArgs e)
         {
-            var position = await locationManager.GetPosition();
-            latitudecord = "Latitude: " + position.Coordinate.Latitude.ToString("0.0000000000");
-            longitudecord = "Longitude: " + position.Coordinate.Longitude.ToString("0.0000000000");
+            //var position = await locationManager.GetPosition();
+            var currentLocation = await locationManager.GetPosition();
+            //latitudecord = "Latitude: " + position.Coordinate.Latitude.ToString("0.0000000000");
+            //longitudecord = "Longitude: " + position.Coordinate.Longitude.ToString("0.0000000000");
+            longitudecord = currentLocation.Coordinate.Point.Position.Longitude.ToString("0.00");
+            latitudecord = currentLocation.Coordinate.Point.Position.Latitude.ToString("0.00");
             textBlock.Text = latitudecord;
             textBlock1.Text = longitudecord;
         }//button_Click
+
+        public class GPSException : Exception
+        {
+            public String Message { get; set; }
+
+            public GPSException(String msg)
+            {
+                Message = msg;
+            }
+        }//GPSException
     }//main
 }//UWP_Project_3
